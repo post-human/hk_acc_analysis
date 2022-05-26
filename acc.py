6a14094e11ccca784156e7b03bf8979c9a5db132
Rocky <18817237197@qq.com>
2022-05-26 21:24:10 +0800

    feat: show analysis & test

--- acc.py
+++ acc.py
@@ -1,45 +1,43 @@
 import datetime
+import json
 import logging
 import os
 import re
 import sqlite3
+import webbrowser
+from pathlib import Path
+from sqlite3 import Connection
 
-import xlrd as xlrd
+import xlrd
 from kivy.app import App
-from kivy.lang import Builder
-from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
+from kivy.uix.dropdown import DropDown
 from kivy.uix.filechooser import FileChooserListView
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.popup import Popup
 from kivy.uix.screenmanager import Screen, ScreenManager
 from pyecharts import options
-from pyecharts.charts import Bar, Page
+# global config
+from pyecharts.charts import Page, Bar
 from xlrd import xldate_as_datetime
 
-# global config
 SUBJECT_CODE_PATTERN = "^\\d{7}$"
 LABEL_OPT = options.LabelOpts(rotate=90, position='middle')
 MONTHS = ["一", "二", "三", "四", "五", "六", "七", "八", "九", "十", "十一", "十二"]
 
 
 def init_log(log_file):
+    log_file = './' + log_file
+    file = Path(log_file)
+    mode = 'a' if file.exists() else 'w'
     logging.basicConfig(  # 针对 basicConfig 进行配置(basicConfig 其实就是对 logging 模块进行动态的调整，之后可以直接使用)
         level=logging.DEBUG,  # INFO 等级以下的日志不会被记录
         format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',  # 日志输出格式
         filename=log_file,  # 日志存放路径(存放在当前相对路径)
-        # filemode=mode,  # 输入模式；如果当前我们文件已经存在，可以使用 'a' 模式替代 'w' 模式
+        filemode=mode,  # 输入模式；如果当前我们文件已经存在，可以使用 'a' 模式替代 'w' 模式
         # 与文件写入的模式相似，'w' 模式为没有文件时创建文件；'a' 模式为追加内容写入日志文件
     )
-
-
-init_log('acc.log')
-
-
-class MainWindow(Screen):
-    pass
-
-
-class AnalysisWindow(Screen):
-    pass
 
 
 def typeof(variate):
@@ -59,6 +57,9 @@ def typeof(variate):
     elif isinstance(variate, set):
         var_type = "set"
     return var_type
+
+
+init_log('acc.log')
 
 
 def save_data(path, filename):
@@ -105,74 +106,185 @@ def save_data(path, filename):
         subject_id_dict = {}
         for result in results:
             subject_id_dict[result[1]] = result[0]
+        now = datetime.datetime.now()
         for subject_code in subject_codes:
             if subject_code not in subject_id_dict:
                 # insert if not exist
                 cursor.execute("insert into hk_subject(acc_id,code,name,create_time) values (?,?,?,?)",
-                               [acc_id, subject_code, subject_dict[subject_code], datetime.datetime.now()])
+                               [acc_id, subject_code, subject_dict[subject_code], now])
                 subject_id = cursor.lastrowid
                 subject_id_dict[subject_code] = subject_id
         insert_list = []
+        update_list = []
+        update_list_log = []
+        data_check_sql = "select hsam.id,hsam.debit,hsam.credit from hk_subject_actual_mount hsam " \
+                         "where hsam.sid=? and hsam.period=?"
+        data_insert_sql = "insert into hk_subject_actual_mount(sid, period, debit, credit, create_time) " \
+                          "values (?,?,?,?,?)"
+        data_update_sql = "update hk_subject_actual_mount set debit=?, credit=?, update_time=? where id=?"
+        log_insert_sql = "insert into hk_subject_actual_mount_log(sam_id, old_data, new_data, create_time) " \
+                         "values (?,?,?,?)"
         for code, v in data_dict.items():
-            data_insert_sql = "insert into hk_subject_actual_mount(sid, period, debit, credit, create_time) " \
-                              "values (?,?,?,?,?)"
             sid = subject_id_dict[code]
             for period, amounts in v.items():
-                data_check_sql = "select hsam.sid,hsam.debit,hsam.credit from hk_subject_actual_mount hsam " \
-                                 "where hsam.sid=? and hsam.period=?"
+
                 cursor = conn.execute(data_check_sql, [sid, period])
                 results = cursor.fetchall()
                 if len(results) == 0:
-                    insert_list.append([sid, period, amounts[0], amounts[1], datetime.datetime.now()])
+                    insert_list.append([sid, period, amounts[0], amounts[1], now])
                 else:
-                    # todo
-                    pass
-            if len(insert_list) > 0:
-                conn.executemany(data_insert_sql, insert_list)
-        print(results)
-        print(subject_id_dict)
+                    # check if modify
+                    sam_id, debit, credit = results[0][0], results[0][1], results[0][2]
+                    if amounts[0] == debit and amounts[1] == credit:
+                        continue
+                    else:
+                        update_list.append([amounts[0], amounts[1], now, sam_id])
+                        update_list_log.append([sam_id, json.dumps({'debit': debit, 'credit': credit}),
+                                                json.dumps({'debit': amounts[0], 'credit': amounts[1]}), now])
+
+        if len(insert_list) > 0:
+            conn.executemany(data_insert_sql, insert_list)
+        if len(update_list) > 0:
+            conn.executemany(data_update_sql, update_list)
+            conn.executemany(log_insert_sql, update_list_log)
 
 
 def get_conn():
     conn = sqlite3.connect('hk.db')
-    conn.set_trace_callback(print)
+    conn.set_trace_callback(logging.debug)
+    # conn.set_trace_callback(print)
     return conn
 
 
-def execute_query(query_sql, conn, param):
+def execute_query(query_sql, conn, param=[]):
     cursor = conn.execute(query_sql, param)
     return cursor.fetchall()
 
 
-class AccManager(ScreenManager):
+def set_btn(button, obj, attr, x):
+    setattr(button, 'text', x)
+    obj[attr] = int(x.split(':')[0])
+
+
+acc_manager = ScreenManager()
+acc_params = {}
+
+
+def switch_window(name, direction=None):
+    acc_manager.current = name
+    if direction is not None:
+        acc_manager.transition.direction = direction
+
+
+class MainWindow(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.container = None
+        layout = GridLayout(cols=1)
+        import_btn = Button(text='Import Excel data', size_hint=(1, 0.5))
+        import_btn.bind(on_press=lambda x: switch_window('file_select'))
+        layout.add_widget(import_btn)
+        analysis_btn = Button(text='Data analysis', size_hint=(1, 0.5))
+        analysis_btn.bind(on_press=lambda x: switch_window('analysis', 'left'))
+        layout.add_widget(analysis_btn)
+        self.add_widget(layout)
 
-    def open_file(self):
-        self.container = BoxLayout(orientation='vertical')
+
+class FileWindow(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
         filechooser = FileChooserListView()
         filechooser.bind(on_selection=lambda x: self.selected(filechooser.selection))
-
-        open_btn = Button(text='open', size_hint=(1, .2))
+        open_btn = Button(text='open', size_hint=(1, 0.05))
         open_btn.bind(on_release=lambda x: self.open(filechooser.path, filechooser.selection))
-
-        self.container.add_widget(filechooser)
-        self.container.add_widget(open_btn)
-        self.get_root_window().add_widget(self.container)
-
-    def open(self, path, filename):
-        if len(filename) > 0 and filename[0].endswith('.xls'):
-            save_data(path, filename)
-        self.get_root_window().remove_widget(self.container)
+        self.add_widget(filechooser)
+        self.add_widget(open_btn)
 
     @staticmethod
-    def selected(filename):
-        logging.info("selected: %s" % filename[0])
+    def open(path, filename):
+        if len(filename) > 0 and filename[0].endswith('.xls'):
+            save_data(path, filename)
+        switch_window('main')
 
-    def show_bar(self):
-        selected_acc_id = 1
-        selected_subject = [2010332, 2010335]
+
+class AnalysisWindow(Screen):
+    conn: Connection
+    accounts = []
+    subjects = []
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.conn = get_conn()
+        self.accounts = execute_query("select ha.acc_id,ha.name from hk_account ha where ha.is_del=0", self.conn, )
+        layout = GridLayout(cols=1)
+        back_btn = Button(text='Go back', size_hint=(1, 0.03))
+        back_btn.bind(on_press=lambda x: switch_window('main', 'right'))
+        account_btn = Button(text='Select account', size_hint=(1, None))
+        account_btn.bind(on_release=self.show_account_dropdown)
+        pre_subject_btn = Button(text='Select subject', size_hint=(1, None))
+        pre_subject_btn.bind(on_release=self.show_pre_subject_dropdown)
+        suf_subject_btn = Button(text='Select subject', size_hint=(1, None))
+        suf_subject_btn.bind(on_release=self.show_suf_subject_dropdown)
+
+        show_chart_btn = Button(text='Show chart', size_hint=(1, 0.05))
+        show_chart_btn.bind(on_press=lambda x: self.show_chart())
+
+        layout.add_widget(back_btn)
+        layout.add_widget(account_btn)
+        layout.add_widget(pre_subject_btn)
+        layout.add_widget(suf_subject_btn)
+        layout.add_widget(show_chart_btn)
+        self.add_widget(layout)
+
+    def show_account_dropdown(self, button):
+        dropdown = DropDown()
+        dropdown.bind(on_select=lambda instance, x: self.set_account(self, button, x))
+
+        for result in self.accounts:
+            btn = Button(text='%s: %s' % (result[0], result[1]), size_hint_y=None, height=15)
+            btn.bind(on_release=lambda btn: dropdown.select(btn.text))
+            # add the button inside the dropdown
+            dropdown.add_widget(btn)
+        dropdown.open(button)
+
+    # get subjects when change account
+    @staticmethod
+    def set_account(self, button, v):
+        set_btn(button, acc_params, 'account_id', v)
+        self.subjects = execute_query(
+            "select hs.code,hs.name from hk_subject hs where hs.is_del=0 and hs.acc_id=? order by hs.code", self.conn,
+            [int(acc_params['account_id'])])
+
+    def show_pre_subject_dropdown(self, button):
+        dropdown = DropDown()
+        dropdown.bind(on_select=lambda instance, x: set_btn(button, acc_params, 'pre_subject_id', x))
+        for result in self.subjects:
+            btn = Button(text='%s: %s' % (result[0], result[1]), size_hint_y=None, height=15)
+            btn.bind(on_release=lambda btn: dropdown.select(btn.text))
+            # add the button inside the dropdown
+            dropdown.add_widget(btn)
+        dropdown.open(button)
+
+    def show_suf_subject_dropdown(self, button):
+        dropdown = DropDown()
+        dropdown.bind(on_select=lambda instance, x: set_btn(button, acc_params, 'suf_subject_id', x))
+        for result in self.subjects:
+            btn = Button(text='%s: %s' % (result[0], result[1]), size_hint_y=None, height=15)
+            btn.bind(on_release=lambda btn: dropdown.select(btn.text))
+            # add the button inside the dropdown
+            dropdown.add_widget(btn)
+        dropdown.open(button)
+
+    @staticmethod
+    def show_chart():
+        if acc_params.get('account_id') is None or acc_params.get('pre_subject_id') is None or acc_params.get(
+                'suf_subject_id') is None:
+            Popup(title='Error', content=Label(text='Miss required parameter'), size_hint=(None, None),
+                  size=(400, 400)).open()
+            return
+        selected_acc_id = acc_params['account_id']
+        selected_subject = [acc_params['pre_subject_id'], acc_params['suf_subject_id']]
+        if selected_subject[1] < selected_subject[0]:
+            selected_subject[0], selected_subject[1] = selected_subject[1], selected_subject[0]
         amount_select_sql = "select hsam.sid,hs.code,hs.name,hsam.period,hsam.debit,hsam.credit " \
                             "from hk_subject_actual_mount hsam inner join hk_subject hs " \
                             "on hsam.sid = hs.sid where hs.code >= ? and hs.code <= ? and hs.acc_id = ? " \
@@ -216,17 +328,22 @@ class AccManager(ScreenManager):
                 bar_credit.add_yaxis(year, item['credit'], label_opts=LABEL_OPT)
             page.add(bar_total, bar_debit, bar_credit)
         # render 会生成本地 HTML 文件，默认会在当前目录生成 render.html 文件
-        # 也可以传入路径参数，如 bar.render("mycharts.html")
-        page.render(datetime.datetime.today().strftime('%Y%m%d'))
+        html = datetime.datetime.today().strftime('%Y%m%d') + '.html'
+        page.render(html)
+        webbrowser.open(html)
 
 
-kv = Builder.load_file('acc.kv')
+acc_manager.add_widget(MainWindow(name='main'))
+acc_manager.add_widget(FileWindow(name='file_select'))
+acc_manager.add_widget(AnalysisWindow(name='analysis'))
+acc_manager.current = 'main'
 
 
-class AccApp(App):
+class HkApp(App):
     def build(self):
-        return kv
+        return acc_manager
 
 
 if __name__ == '__main__':
-    AccApp().run()
+    HkApp().run()
+# pyinstaller -F -w acc.py
